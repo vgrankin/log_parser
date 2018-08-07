7087ed2569f0cbe9c295bc630efe53c7299a76f0
unknown <phpcoder@inbox.lv>
2018-08-07 19:19:26 +0300

    Config update

--- src/com/ef/Config.java
+++ src/com/ef/Config.java
@@ -4,9 +4,5 @@ public class Config
 {
     public static final String JDBC_URL = "jdbc:mysql://127.0.0.1:3306/ef";
     public static final String DB_USERNAME = "root";
-    public static final String DB_PASSWORD = "";     
-    
-    public static final String TEST_JDBC_URL = "jdbc:mysql://127.0.0.1:3306/ef";
-    public static final String TEST_DB_USERNAME = "root";
-    public static final String TEST_DB_PASSWORD = "";         
+    public static final String DB_PASSWORD = "";
 }
